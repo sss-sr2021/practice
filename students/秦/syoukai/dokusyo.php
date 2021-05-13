90f1db00807a41e76136da4d14b811cdd92ab8a0
秦 <hata@sr-co.co.jp>
2021-05-13 14:24:26 +0900

    自己紹介

---
+++ students/秦/syoukai/dokusyo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="Shift_JIS"/>
+		<title>読書</title>
+        <link rel ="stylesheet" href="dokusyo.css"/>
+
+	</head>
+    <body>
+        <h1>～読書～</h1>
+        <div class="dokusyo">
+            <header>
+            <p>どれくらい好きなのか？</br>→書店に入ったら１~２時間出てきません。</p>
+            <p>ジャンル：青春モノから推理小説まで幅広く読みます。</p>
+            </header>
+                <p>おすすめの小説↓</p>
+                    <div class="a">
+                        <a href="https://www.amazon.co.jp/%E3%82%86-1-1-%E5%83%95%E3%81%A8%E5%90%9B%E3%81%AE365%E6%97%A5-%E3%83%9D%E3%83%97%E3%83%A9%E6%96%87%E5%BA%AB%E3%83%94%E3%83%A5%E3%82%A2%E3%83%95%E3%83%AB/dp/4591162575/ref=sr_1_1?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=27MNE5X5VA6OO&dchild=1&keywords=%E5%83%95%E3%81%A8%E5%90%9B%E3%81%AE365%E6%97%A5&qid=1619936741&sprefix=%E5%83%95%E3%81%A8%E5%90%9B%E3%81%AE365%E6%97%A5%2Caps%2C822&sr=8-1"><img src="365.jpg" width25%/></a>
+                        <p>📚僕と君の365日</p>
+                    </div>
+                    <div class="b">
+                        <a href="https://www.amazon.co.jp/%E7%9B%A3%E7%8D%84%E3%81%AB%E7%94%9F%E3%81%8D%E3%82%8B%E5%90%9B%E3%81%9F%E3%81%A1%E3%81%B8-%E3%83%A1%E3%83%87%E3%82%A3%E3%82%A2%E3%83%AF%E3%83%BC%E3%82%AF%E3%82%B9%E6%96%87%E5%BA%AB-%E6%9D%BE%E6%9D%91-%E6%B6%BC%E5%93%89/dp/404913375X/ref=sr_1_1?adgrpid=122732831441&dchild=1&hvadid=506499916002&hvdev=c&hvqmt=e&hvtargid=kwd-1069863767440&hydadcr=1789_10144706&jp-ad-ap=0&keywords=%E7%9B%A3%E7%8D%84%E3%81%AB%E7%94%9F%E3%81%8D%E3%82%8B%E5%90%9B%E3%81%9F%E3%81%A1%E3%81%B8&qid=1619937027&sr=8-1"><img src="kanngoku.jpg" width25%/></a>
+                        <p>📚監獄に生きる君たちへ</p>
+                    </div>
+            <footer><a href="syoukai2.php">ホーム</a></footer>
+        </div>
+    </body>
+</html>
---
+++ students/秦/syoukai/rikuzyou.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="Shift_JIS"/>
+		<title>陸上競技</title>
+        <link rel ="stylesheet" href="rikuzyou.css"/>
+	</head>
+    <body>
+        <h1>～陸上競技～</h1>
+        <div class="rikuzyou">
+            <p>経歴：小学5年生～高校3年生</p>
+            <p>役割：中学生＝キャプテン、高校生＝副キャプテン
+            </p>
+            <p>種目：長距離</p>
+            <p>競技</p>
+            <ul>
+                <li>８００M</li>
+                <li>１５００M</li>
+                <li>３０００M</li>
+                <li>３０００M <a href="https://ja.wikipedia.org/wiki/3000%E3%83%A1%E3%83%BC%E3%83%88%E3%83%AB%E9%9A%9C%E5%AE%B3"><span class="marker">SC</span></a></li>
+                <li>５０００M</li>
+                <li>駅伝</li>
+            </ul>
+            <p>大会成績</p>
+            <a href="http://www.haaa.jp/~himeji/H28/kirokukai/pc/rel218.html"><img src="rikuzyou.jpeg"/></a>
+            <p><a href="syoukai2.php">ホーム</a></p>
+        </div>
+    </body>
+</html>
